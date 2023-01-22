fe16292c9749a07cdd012e75d1a868a5e1a7bfb0
TurBo-official <TurBo.infomation@gmail.com>
2023-01-23 01:50:23 +0900

    add ease in animation

---
+++ js/windowAnim.js
@@ -0,0 +1,31 @@
+//scroll_effect
+$(window).ready(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_up');
+  var scrollAnimationFunc = function () {
+
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});
+
+//scroll_effect
+$(window).ready(function () {
+  var scrollAnimationElm = document.querySelectorAll('.scroll_right');
+  var scrollAnimationFunc = function () {
+
+    for (var i = 0; i < scrollAnimationElm.length; i++) {
+      var triggerMargin = 100;
+      if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+        scrollAnimationElm[i].classList.add('on');
+      }
+    }
+  }
+  window.addEventListener('load', scrollAnimationFunc);
+  window.addEventListener('scroll', scrollAnimationFunc);
+});

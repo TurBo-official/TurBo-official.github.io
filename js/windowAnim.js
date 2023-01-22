58f053a4aa041a9fd30067de37b6b04590bb3b97
TurBo-official <TurBo.infomation@gmail.com>
2023-01-23 04:19:26 +0900

    add ease in animation DOM fixed

--- js/windowAnim.js
+++ js/windowAnim.js
@@ -23,7 +23,7 @@ $(window).ready(function () {
   var scrollAnimationFunc = function () {
 
     for (var i = 0; i < scrollAnimationElm.length; i++) {
-      var triggerMargin = 1000;
+      var triggerMargin = 100;
       if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
         scrollAnimationElm[i].classList.add('on');
       }
